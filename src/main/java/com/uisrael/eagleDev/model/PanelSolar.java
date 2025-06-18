7d6d3b1dedfa2fb18fb0987e540b6eba6be84904
GabMF-96 <e1720115730@uisrael.edu.ec>
2025-06-17 23:07:01 -0500

    PanelSolar_Cambio_1

--- src/main/java/com/uisrael/eagleDev/model/PanelSolar.java
+++ src/main/java/com/uisrael/eagleDev/model/PanelSolar.java
@@ -1,4 +1,16 @@
 package com.uisrael.eagleDev.model;
 
-public class PanelSolar {
+public class PanelSolar extends FuenteEnergiaBase {
+    public PanelSolar() {
+        super("Panel Solar");
+    }
+
+    public double calcularCosto(double kWh) {
+        return 0.0;
+    }
+
+    public double calcularEmisiones(double kWh) {
+        return 0.0;
+    }
+
 }
