63c8e22249fd5872b9e4b6462da12b3e15b0e764
GabMF-96 <e1720115730@uisrael.edu.ec>
2025-06-17 23:09:26 -0500

    RedElectrica_ Cambio_1

--- src/main/java/com/uisrael/eagleDev/model/RedElectrica.java
+++ src/main/java/com/uisrael/eagleDev/model/RedElectrica.java
@@ -1,4 +1,15 @@
 package com.uisrael.eagleDev.model;
 
-public class RedElectrica {
+public class RedElectrica extends FuenteEnergiaBase {
+    public RedElectrica() {
+        super("Red Eléctrica");
+    }
+
+    public double calcularCosto(double kWh) {
+        return kWh * 0.18;
+    }
+
+    public double calcularEmisiones(double kWh) {
+        return kWh * 0.4;
+    }
 }
