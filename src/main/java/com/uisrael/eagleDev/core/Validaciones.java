520e85b284784080493afb7ff3f05a4005859b49
MayMendez <e1728736602@uisrael.edu.ec>
2025-06-18 19:05:40 -0500

    Modificacion en clase validacion

--- src/main/java/com/uisrael/eagleDev/core/Validaciones.java
+++ src/main/java/com/uisrael/eagleDev/core/Validaciones.java
@@ -2,4 +2,17 @@ package com.uisrael.eagleDev.core;
 
 public class Validaciones {
     
+    public static boolean ValidarNombre(String nombre){
+        return nombre.matches("[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ ]+");
+    }
+
+    public static boolean ValidarConsumo(String input){
+        try{
+            double valor = Double.parseDouble(input);
+            return valor >0;
+        }catch (NumberFormatException exception){
+            return false;
+        }
+    }
+
 }
